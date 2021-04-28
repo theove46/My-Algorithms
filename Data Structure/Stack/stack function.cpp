6eba12240b10aaf37776630ba571252fd5be72ad
Md. Ohiduzzaman Ove <57717227+theove46@users.noreply.github.com>
2021-04-28 23:19:14 +0600

    Add files via upload

---
+++ Data Structure/Stack/stack function.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // stack push pop
+    /*
+    stack <int> m;
+    m.push(1);
+    m.push(2);
+    m.push(3);
+    m.push(4);
+    // Stack becomes 1, 2, 3, 4
+
+    m.pop();
+    m.pop();
+    // Stack becomes 1, 2
+
+    cout << m.top() << endl;
+    cout << m.size() << endl;
+
+    while (!m.empty())
+        {
+        cout << m.top() << " ";
+        m.pop();}
+    */
+
+
+    // vector push pop
+    /*
+    int count = 0;
+    vector<int> m;
+
+    m.push_back(1);
+    m.push_back(2);
+    m.push_back(4);
+    m.push_back(5);
+    m.push_back(6);
+
+    for (int i = 0; i < m.size(); ++i)
+      cout << m[i] << " ";
+    cout << endl;
+
+    m.pop_back();
+    m.pop_back();
+    m.pop_back();
+
+    for (int i = 0; i < m.size(); ++i)
+      cout << m[i] << " ";
+    cout << endl;
+
+    while (!m.empty()) {
+        count++;
+        m.pop_back();
+    }
+    cout << count;
+    */
+
+    return 0;
+}
+
