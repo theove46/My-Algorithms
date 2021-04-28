45075777843ff0a2c6374f6f1a1db239f848a969
Md. Ohiduzzaman Ove <57717227+theove46@users.noreply.github.com>
2021-04-28 23:48:36 +0600

    Update Dijkstras iterative.cpp

--- Graph/Dijkstras iterative.cpp
+++ Graph/Dijkstras iterative.cpp
@@ -1,14 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Number of vertices in the graph
 #define V 9
 
-// A utility function to find the vertex with minimum distance value, from
-// the set of vertices not yet included in shortest path tree
 int minDistance(int dist[], bool sptSet[])
 {
-    // Initialize min value
     int min = INT_MAX, min_index;
 
     for (int v = 0; v < V; v++)
@@ -20,7 +16,6 @@ int minDistance(int dist[], bool sptSet[])
     return min_index;
 }
 
-// A utility function to print the constructed distance array
 int printSolution(int dist[])
 {
     printf("Vertex \t\t Distance from Source\n");
@@ -28,47 +23,35 @@ int printSolution(int dist[])
         printf("%d \t\t %d\n", i, dist[i]);
 }
 
-// Function that implements Dijkstra's single source shortest path algorithm
-// for a graph represented using adjacency matrix representation
 void dijkstra(int graph[V][V], int src)
 {
-    int dist[V]; // The output array.  dist[i] will hold the shortest
-    // distance from src to i
+    int dist[V];
 
-    bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
-    // path tree or shortest distance from src to i is finalized
+    bool sptSet[V]; 
 
-    // Initialize all distances as INFINITE and stpSet[] as false
     for (int i = 0; i < V; i++)
         dist[i] = INT_MAX, sptSet[i] = false;
 
-    // Distance of source vertex from itself is always 0
     dist[src] = 0;
 
     // Find shortest path for all vertices
-    for (int count = 0; count < V - 1; count++) {
-        // Pick the minimum distance vertex from the set of vertices not
-        // yet processed. u is always equal to src in the first iteration.
+    for (int count = 0; count < V - 1; count++) 
+    {
         int u = minDistance(dist, sptSet);
-
-        // Mark the picked vertex as processed
         sptSet[u] = true;
 
-        // Update dist value of the adjacent vertices of the picked vertex.
         for (int v = 0; v < V; v++)
-
-            // Update dist[v] only if is not in sptSet, there is an edge from
-            // u to v, and total weight of path from src to  v through u is
-            // smaller than current value of dist[v]
+        {
             if((sptSet[v]==false) && (graph[u][v]!=0) && (dist[u] != INT_MAX) && (dist[u] + graph[u][v] < dist[v]))
                 {dist[v] = dist[u] + graph[u][v];
                  cout << "dis u- " << dist[u] << " dist V-" << dist[v] << " v-" << v << " graph-" << graph[u][v] << endl;
                  }
+        }
     }
 
-    // print the constructed distance array
     printSolution(dist);
 }
+
 int main()
 {
     //cout << "How many elements: ";
